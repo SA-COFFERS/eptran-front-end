74115812b78dc370eba7f175dd9e9385cf3ae4af
Matthaus <matthauspaula@gmail.com>
2024-06-25 09:16:22 -0300

    finish secondForm

--- src/components/Register/SecondForm/SecondForm.jsx
+++ src/components/Register/SecondForm/SecondForm.jsx
@@ -10,14 +10,14 @@ function SecondForm() {
       <p className="main-text">Complete o cadastro</p>
       <div className="name">
         <p className="form-text">Nome Completo:</p>
-        <input className="dark-input" type="text" id="name" placeholder="  Nome completo" />
+        <input className="dark-input" type="text" id="name" placeholder="  Fulano de Tal" />
       </div>
       <div className="birth">
         <p className="form-text">Data de nascimento:</p>
         <input className="dark-input" type="date" id="birthday" />
       </div>
       <div className="sex-options-div">
-        <p className="form-text">Informe seu sexo:</p>
+        <p className="form-text">Sexo:</p>
         <section className="sex-options-section">
           <div className="sex-option">
             <input className="dark-input" type="radio" id="male" name="gender" value="male" />
@@ -33,15 +33,25 @@ function SecondForm() {
           </div>
         </section>
       </div>
+      <div className="school">
+        <p className="form-text">Escolaridade</p>
+        <select id="school" className="dark-input">
+          <option value="lvl2">Fundamental 1</option>
+          <option value="lvl3">Fundamental 2</option>
+          <option value="lvl4">Ensino Médio</option>
+        </select>
+      </div>
 
-      <select id="school" className="dark-input">
-        <option value="lvl1">Escolaridade:</option>
-        <option value="lvl2">Fundamental 1</option>
-        <option value="lvl3">Fundamental 2</option>
-        <option value="lvl4">Ensino Médio</option>
-      </select>
+      <div className="buttons">
+        <button className="back-button">Voltar</button>
+        <button className="color-button finish-button"><a href="../index.html" className="finish">Finalizar</a></button>
+      </div>
 
-      <button className="color-button"><a href="../index.html" className="finish">Finalizar</a></button>
+      <a to="/login" className="login-link">
+        <p>
+          Já possui uma conta?/Login
+        </p>
+      </a>
     </form>
   );
 }
